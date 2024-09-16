c745523f2c26a458a56d7620001d21eb51b35ba8
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-09-16 14:57:27 +0530

    Timer

---
+++ Timer.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+const SimpleTimer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  const startTimer = () => {
+    setIsActive(true);
+  };
+
+  const stopTimer = () => {
+    setIsActive(false);
+  };
+
+  const resetTimer = () => {
+    setSeconds(0);
+    setIsActive(false);
+  };
+
+  return (
+    <div>
+      <h1>{seconds}s</h1>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resetTimer}>Reset</button>
+    </div>
+  );
+};
+
+export default SimpleTimer;
